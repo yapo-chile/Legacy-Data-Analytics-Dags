efbfbd45e64ab6d7c66fdc868a994e1781178e5e
andres <andres.vivallo@yapo.cl>
2022-07-06 10:30:50 -0400

    pentaho and dockerhost DAGs templates ready

--- pentaho-template.py
+++ pentaho-template.py
@@ -1,45 +1,37 @@
 from __future__ import print_function
 
-from datetime import datetime, timedelta
+from datetime import datetime
 
+# Common methods
 from lib.slack_msg import slack_msg_body
-#TODO incorporar get_date common
+from lib.get_dates import get_date
 
 from airflow import models
 from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
 from airflow.contrib.operators.ssh_operator import SSHOperator
 from airflow.contrib.hooks.ssh_hook import SSHHook
 
+# DEFINE INIT PARAMS
+# Dag
+dag_name = "<DAG_NAME>"
+dag_tags =  ["testing", "ETL", "schedule", "pentaho", "git: legacy/bi-insight", "input: dwh", "output: dwh"]
+# Schedule interal
+schedule_interval = "<SCHEDULE_INTERVAL>"  # This example scheduled at daily 6 AM: "0 6 * * *"
+# Slack msg
+riskiness = "<RISKINESS>"  # High, Medium or Low
+utility = "<UTILITY_OF_DAG>"  # Example: "This etl generates ... data in DWH."
+
 SLACK_CONN_ID = 'slack'
 sshHook = SSHHook(ssh_conn_id="ssh_public_pentaho")
 
-# Define schedule interal
-schedule_interval = '0 6 * * *'  # This example scheduled at daily 6 AM
-# Define slack msg params
-riskiness = "Medium"  # or High or Low
-utility = "This etl generates ... data in DWH."
 
 default_args = {
     # The start_date describes when a DAG is valid / can be run. Set this to a
     # fixed point in time rather than dynamically, since it is evaluated every
     # time a DAG is parsed. See:
     # https://airflow.apache.org/faq.html#what-s-the-deal-with-start-date
-    "start_date": datetime(2022, 7, 5),
+    "start_date": datetime(2022, 7, 6),
 }
-
-
-# Calculate the date
-def get_date():
-    start_date = "{{ dag_run.conf['start_date'] if dag_run.conf and 'start_date' in dag_run.conf else False}}"
-    end_date = "{{ dag_run.conf['end_date'] if dag_run.conf and 'end_date' in dag_run.conf else False}}"
-    if start_date and end_date:
-        date = {"start_date": start_date, "end_date": end_date}
-    else:
-        execution_date = "{{ dag_run.logical_date }}"
-        execution_date = execution_date - timedelta(days=1)
-        execution_date = execution_date.date().strftime('%Y-%m-%d')
-        date = {"start_date": execution_date, "end_date": execution_date}
-    return date
 
 
 def task_fail_slack_alert(context):
@@ -59,27 +51,21 @@ def task_fail_slack_alert(context):
 
 
 with models.DAG(
-    "trigger_bi-insight-dw_blocketdb-kpis_operacionales",
-    tags=[
-        "production",
-        "ETL",
-        "trigger",
-        "core",
-        "git: legacy/bi-insight",
-        "input: dwh",
-        "output: dwh",
-    ],
+    dag_name,
+    tags=dag_tags,
     schedule_interval=schedule_interval,
     default_args=default_args,
     max_active_runs=1,
     on_failure_callback=task_fail_slack_alert
 ) as dag:
 
-    run_kpi_operacionales = SSHOperator(
-        task_id="run_kpi_operacionales",
+    run_ssh = SSHOperator(
+        task_id="example_task_id",
         ssh_hook=sshHook,
-        command="sh /opt/dw_schibsted/yapo_bi/dw_blocketdb/kpis_operacionales/run_jb_kpis_operacionales.sh "
+        # Example of command:
+        # sh /opt/dw_schibsted/yapo_bi/dw_blocketdb/kpis_operacionales/run_jb_kpis_operacionales.sh
+        command="sh /opt/dw_schibsted/yapo_bi/dw_blocketdb/kpis_operacionales/run_jb_kpis_operacionales.sh "  # You need add a space at the end of the command, to avoid error: Jinja template not found
     )
 
-    run_kpi_operacionales
+    run_ssh
 
