39abbe3e93953ab20a1b0fac2f050500f297316c
cdreves <carlos.dreves@yapo.cl>
2022-07-11 16:35:40 -0400

    update etls and triggers

---
+++ dockerhost/core/legacy-data-content-core-ad-sellers.py
@@ -0,0 +1,108 @@
+from __future__ import print_function
+
+import logging
+from datetime import datetime
+
+from airflow import models
+from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
+from airflow.models.variable import Variable
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from lib.get_dates import get_date
+
+# Common methods
+from lib.slack_msg import slack_msg_body
+
+# DEFINE INIT PARAMS
+# Dag
+dag_name = "legacy_data-content_ad_sellers"
+dag_tags = [
+    "production",
+    "ETL",
+    "dockerhost-pentaho",
+    "legacy",
+    "git: legacy/data-content",
+    "input: pending",
+    "output: pending",
+]
+
+# Schedule interal
+schedule_interval = "10 4 * * *"
+# Slack msg
+riskiness = "Medium"
+utility = "Daily this pipeline makes available into DWH the data related to sellers of ours ads whatever its classification private or professional as well as a detail available related to sellers pro in order to know in which categories a seller is professional"
+
+SLACK_CONN_ID = "slack"
+
+
+default_args = {
+    # The start_date describes when a DAG is valid / can be run. Set this to a
+    # fixed point in time rather than dynamically, since it is evaluated every
+    # time a DAG is parsed. See:
+    # https://airflow.apache.org/faq.html#what-s-the-deal-with-start-date
+    "start_date": datetime(2022, 7, 6),
+}
+
+
+def task_fail_slack_alert(context):
+    slack_msg = slack_msg_body(
+        context,
+        riskiness=riskiness,
+        utility=utility,
+    )
+    failed_alert = SlackWebhookOperator(
+        task_id="failed_job_alert",
+        http_conn_id=SLACK_CONN_ID,
+        message=slack_msg,
+        username="airflow",
+        dag=dag,
+    )
+    return failed_alert.execute(context=context)
+
+
+with models.DAG(
+    dag_name,
+    tags=dag_tags,
+    schedule_interval=schedule_interval,
+    default_args=default_args,
+    max_active_runs=1,
+    on_failure_callback=task_fail_slack_alert,
+) as dag:
+
+    trigger_dag_legacy_trigger_data_content_ad_sellers = TriggerDagRunOperator(
+        task_id="task_trigger_dag_legacy_trigger_data_content_ad_sellers",
+        trigger_dag_id="legacy_trigger_data-content_ad_sellers",
+        wait_for_completion=True,
+        do_xcom_push=False,
+    )
+    trigger_dag_legacy_trigger_data_content_ads_created_daily = TriggerDagRunOperator(
+        task_id="task_trigger_dag_legacy_trigger_data_content_ads_created_daily",
+        trigger_dag_id="legacy_trigger_data-content_ads_created_daily",
+        wait_for_completion=True,
+        do_xcom_push=False,
+    )
+    trigger_dag_legacy_trigger_data_revenues_incremental_product_order = TriggerDagRunOperator(
+        task_id="task_trigger_trigger_dag_legacy_trigger_data_revenues_incremental_product_order",
+        trigger_dag_id="legacy_trigger_data-revenues_incremental_product_order",
+        wait_for_completion=True,
+        do_xcom_push=False,
+    )
+    trigger_dag_legacy_trigger_data_revenues_store_purshases = TriggerDagRunOperator(
+        task_id="task_trigger_dag_legacy_trigger_data_revenues_store_purshases",
+        trigger_dag_id="legacy_trigger_data-revenues_store-purshases",
+        wait_for_completion=True,
+        do_xcom_push=False,
+    )
+    trigger_dag_legacy_trigger_bi_insight_dw_blocketdb_etl_incremental_automatico = TriggerDagRunOperator(
+        task_id="task_trigger_dag_legacy_trigger_bi_insight_dw_blocketdb_etl_incremental_automatico",
+        trigger_dag_id="legacy_trigger_bi-insight-dw-blocketdb_etl_incremental_automatico",
+        wait_for_completion=True,
+        do_xcom_push=False,
+    )
+
+    (
+        trigger_dag_legacy_trigger_data_content_ad_sellers
+        >> trigger_dag_legacy_trigger_data_content_ads_created_daily
+        >> trigger_dag_legacy_trigger_data_revenues_incremental_product_order
+        >> trigger_dag_legacy_trigger_data_revenues_store_purshases
+        >> trigger_dag_legacy_trigger_bi_insight_dw_blocketdb_etl_incremental_automatico
+    )
