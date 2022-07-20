70488e00709117b16ad80857bab38b02b8cd4348
cdreves <carlos.dreves@yapo.cl>
2022-07-20 12:08:21 -0400

    add new etl legacy without rundeck dependency

---
+++ dockerhost/content/legacy-data-content-new-approved-ads.py
@@ -0,0 +1,100 @@
+import logging
+from datetime import datetime
+
+from airflow import models
+from airflow.contrib.hooks.ssh_hook import SSHHook
+from airflow.contrib.operators import ssh_operator
+from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
+from airflow.models.variable import Variable
+from airflow.operators import python_operator
+from lib.get_dates import get_date
+
+# Common methods
+from lib.slack_msg import slack_msg_body
+
+# DEFINE INIT PARAMS
+# Dag
+dag_name = "legacy_data-content_new_approved_ads"
+dag_tags = [
+    "production",
+    "ETL",
+    "schedule",
+    "dockerhost",
+    "legacy",
+    "git: legacy/data-content",
+    "input: pending",
+    "output: pending",
+]
+# Docker image
+docker_image = (
+    "containers.mpi-internal.com/yapo/data-content-peak-new-approved-ads:latest"
+)
+# Schedule interal
+schedule_interval = "0 11 * * *"
+# Slack msg
+riskiness = "Medium"
+utility = "Legacy etl related to peak new approved ads"
+
+sshHook = SSHHook(ssh_conn_id="ssh_public_pentaho")
+connect_dockerhost = Variable.get("CONNECT_DOCKERHOST")
+SLACK_CONN_ID = "slack"
+
+
+default_args = {
+    # The start_date describes when a DAG is valid / can be run. Set this to a
+    # fixed point in time rather than dynamically, since it is evaluated every
+    # time a DAG is parsed. See:
+    # https://airflow.apache.org/faq.html#what-s-the-deal-with-start-date
+    "start_date": datetime(2022, 7, 20),
+}
+
+
+def task_fail_slack_alert(context):
+    slack_msg = slack_msg_body(
+        context,
+        riskiness=riskiness,
+        utility=utility,
+    )
+    failed_alert = SlackWebhookOperator(
+        task_id="failed_job_alert",
+        http_conn_id=SLACK_CONN_ID,
+        message=slack_msg,
+        username="airflow",
+        dag=dag,
+    )
+    return failed_alert.execute(context=context)
+
+
+with models.DAG(
+    dag_name,
+    tags=dag_tags,
+    schedule_interval=schedule_interval,
+    default_args=default_args,
+    max_active_runs=1,
+    on_failure_callback=task_fail_slack_alert,
+) as dag:
+
+    def call_ssh(**kwargs):
+        dates = get_date(**kwargs)
+        logging.info(f"detected days: {dates}")
+        command_line = f"""-v /home/bnbiuser/secrets/dw_db:/app/db-secret \
+                -e APP_DB_SECRET=/app/db-secret \
+                {docker_image} \
+                -date_from={dates['start_date']} \
+                -date_to={dates['end_date']}"""
+        call = ssh_operator.SSHOperator(
+            task_id="task_new_approved_ads",
+            ssh_hook=sshHook,
+            command=f"""{connect_dockerhost} <<EOF \n
+                        sudo docker pull {docker_image} \n
+                        sudo docker run {command_line}""",
+        )
+        call.execute(context=kwargs)
+
+    new_approved_ads = python_operator.PythonOperator(
+        task_id="task_new_approved_ads",
+        provide_context=True,
+        python_callable=call_ssh,
+    )
+
+    new_approved_ads
